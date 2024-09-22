24bd3c566061b04e6540498db9b3e18a7864dde0
Kostiantyn Konovalenko <kostiantyn.konovalenko@nixs.com>
2024-09-22 21:24:50 +0300

    feature: added environments

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+export const config = {
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  HOST: process.env.HOST || 'localhost',
+  PORT: process.env.PORT ? +process.env.PORT : 3000,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
-import dotenv from 'dotenv'
+import { config } from './config'
 import express, { Express, Request, Response } from 'express'
 
-dotenv.config()
-
 const app: Express = express()
-const PORT = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TS')
 })
 
-app.listen(PORT, () => {
-  console.log(`[server]: Server is running at http://localhost:${PORT}`)
+app.listen(config.PORT, config.HOST, () => {
+  console.log(`[server]: Server is running at http://${config.HOST}:${config.PORT}`)
 })
